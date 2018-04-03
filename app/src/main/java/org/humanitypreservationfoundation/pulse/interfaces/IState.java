d3948bf3580ddc49468f8193555f312a00ba8fad
Anna Harrison <annaharri89@gmail.com>
2018-04-03 10:52:52 -0400

    Updated interfaces.

--- app/src/main/java/org/humanitypreservationfoundation/pulse/interfaces/IState.java
+++ app/src/main/java/org/humanitypreservationfoundation/pulse/interfaces/IState.java
@@ -5,14 +5,26 @@ import android.graphics.Region;
 
 import com.sdsmdg.harjot.vectormaster.models.PathModel;
 
+import org.humanitypreservationfoundation.pulse.classes.Resource;
+import org.humanitypreservationfoundation.pulse.enums.StateEnum;
+
+import java.util.List;
+
 /**
  * Created by ETASpare on 1/23/2018.
  */
 
 public interface IState {
     String getCode();
+    String getName();
+    StateEnum getEnum();
     Path getPath();
     PathModel getPathModel();
     Region getRegion();
     void setFillColor(String color);
+    void setResources(List<Resource> resourceList);
+    List<Resource> getChildAbuseResources();
+    List<Resource> getBullyingResources();
+    List<Resource> getDomesticViolenceResources();
+    List<Resource> getAllResources();
 }
--- app/src/main/java/org/humanitypreservationfoundation/pulse/interfaces/ITimeZone.java
+++ app/src/main/java/org/humanitypreservationfoundation/pulse/interfaces/ITimeZone.java
@@ -2,10 +2,13 @@ package org.humanitypreservationfoundation.pulse.interfaces;
 
 import android.graphics.Region;
 
+import org.humanitypreservationfoundation.pulse.classes.Resource;
 import org.humanitypreservationfoundation.pulse.classes.State;
+import org.humanitypreservationfoundation.pulse.enums.StateEnum;
 import org.humanitypreservationfoundation.pulse.enums.TimeZoneEnum;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by ETASpare on 1/27/2018.
@@ -15,9 +18,14 @@ public interface ITimeZone {
     List<State> getStates();
     String getName();
     String getCode();
+    String getColorCode();
     TimeZoneEnum getEnum();
     String getDescription();
     Region getRegion();
     void changeFillColor(String qualifier);
-    //Boolean checkStatesForTap(int x, int y); TODO remove when confirmed unnecessary
+    void setResources(Map<StateEnum, List<Resource>> resourceMap);
+    List<Resource> getStateChildAbuseResources(String stateCode);
+    List<Resource> getStateBullyingResources(String stateCode);
+    List<Resource> getStateDomesticViolenceResources(String stateCode);
+    List<Resource> getAllStateResources(String stateCode);
 }
