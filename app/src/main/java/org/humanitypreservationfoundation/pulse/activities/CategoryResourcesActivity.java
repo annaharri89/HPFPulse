8ad32348eea464006260196ebea79ae72c1b8650
Anna Harrison <annaharri89@gmail.com>
2018-04-04 15:48:59 -0400

    Removed check for PST timezone, making the CategoryResourcesActivity work for all time zones.

--- app/src/main/java/org/humanitypreservationfoundation/pulse/activities/CategoryResourcesActivity.java
+++ app/src/main/java/org/humanitypreservationfoundation/pulse/activities/CategoryResourcesActivity.java
@@ -26,28 +26,30 @@ public class CategoryResourcesActivity extends ResourcesActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_resources);
 
-        List<Resource> resources = new ArrayList<Resource>();
-        if (mTimeZone.getEnum().equals(TimeZoneEnum.PST)) { //todo remove PST check once db is set up and hooked up to real data
-            if (mActivityName.equals(Config.categories.CHILD_ABUSE)) {
+        List<Resource> resources = new ArrayList<>();
+        switch (mActivityName) {
+            case Config.categories.CHILD_ABUSE:
                 for (State state: mTimeZone.getStates()) {
                     resources.addAll(mTimeZone.getStateChildAbuseResources(state.getCode()));
                 }
-            } else if (mActivityName.equals(Config.categories.BULLYING)) {
+                break;
+            case Config.categories.BULLYING:
                 for (State state: mTimeZone.getStates()) {
                     resources.addAll(mTimeZone.getStateBullyingResources(state.getCode()));
                 }
-            } else if (mActivityName.equals(Config.categories.DOMESTIC_VIOLENCE)) {
+                break;
+            case Config.categories.DOMESTIC_VIOLENCE:
                 for (State state: mTimeZone.getStates()) {
                     resources.addAll(mTimeZone.getStateDomesticViolenceResources(state.getCode()));
                 }
-            }
-
-            if (resources.size() > 0) {
-                CategoryResourceListAdapter adapter = new CategoryResourceListAdapter(this, resources);
-                mListView = (ListView) findViewById(R.id.category_resources_list);
-                mListView.setAdapter(adapter);
-            }
-            //todo do i need this: mListView.invalidateViews();
+                break;
         }
+
+        if (resources.size() > 0) {
+            CategoryResourceListAdapter adapter = new CategoryResourceListAdapter(this, resources);
+            mListView = (ListView) findViewById(R.id.category_resources_list);
+            mListView.setAdapter(adapter);
+        }
+        //todo do i need this: mListView.invalidateViews();
     }
 }
